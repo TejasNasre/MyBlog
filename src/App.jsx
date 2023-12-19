dc0125f46f21ee1c330fff22cc34209d5730c738
tejas <tejasnasre120@gmail.com>
2023-12-19 17:43:47 +0530

    React Redux Setup And Make Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
-import conf from './conf/conf.js'
+import conf from "./conf/conf.js";
+import {Header,Footer} from "./components/index.js";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
 
 function App() {
-  console.log(conf.appwriteUrl)
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispath = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispath(login({ userData }));
+        } else {
+          dispath(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
     <>
-    <h1>Welcome Tejas </h1>
+      <h1 className="min-h-screen flex flex-wrap content-between">
+        <Header/>
+        <Footer/>
+      </h1>
     </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
